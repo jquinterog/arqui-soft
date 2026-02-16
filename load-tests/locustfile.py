6bd2d207cdc73ec7d9e3ef021622480df31d7fce
Jose Quintero <joseantonioq9@gmail.com>
2026-02-15 19:34:13 -0500

    Adding dockerfile for orders manager and making locust load stocastic

--- load-tests/locustfile.py
+++ load-tests/locustfile.py
@@ -1,6 +1,10 @@
+import time
+import random
 from locust import HttpUser, task
 
 class HelloWorldUser(HttpUser):
     @task
     def hello_world(self):
         self.client.get("/")
+        random_timeout_ms = random.randint(0, 119999)
+        time.sleep(random_timeout_ms / 1000)
