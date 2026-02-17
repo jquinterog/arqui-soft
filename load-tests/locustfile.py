e26725cac87c0f28cfb3baeeda6bc8d59d9d746c
Jose Quintero <joseantonioq9@gmail.com>
2026-02-16 21:36:00 -0500

    Fixing k8s env

--- load-tests/locustfile.py
+++ load-tests/locustfile.py
@@ -4,7 +4,18 @@ from locust import HttpUser, task
 
 class HelloWorldUser(HttpUser):
     @task
-    def hello_world(self):
-        self.client.get("/")
-        random_timeout_ms = random.randint(0, 119999)
-        time.sleep(random_timeout_ms / 1000)
+    def create_order(self):
+        payload = {
+            "tipo": "venta",
+            "activo": "string",
+            "cantidad": 1,
+            "precio": 1,
+            "cliente_id": "string"
+        }
+
+        self.client.post(
+            "/ordenes",
+            json=payload,  # Automatically sets Content-Type: application/json
+        )
+        # random_timeout_ms = random.randint(0, 119999)
+        # time.sleep(random_timeout_ms / 1000)
